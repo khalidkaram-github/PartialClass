440963ede966781a785e2380da78ec8375ef7e5e
khalid karam <khalid.tasks@gmail.com>
2024-09-10 20:53:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // object intializer
+            //User user = new() { Age = 10, Name = "" };
+            //var user = new { Age = 10, Name = "" };
+            //User user = new();
+
+
+
+        }
+    }
+
+    //partal classes
+
+    partial class User
+    {
+        public int Age { get; set; }
+        public void Welcome() { }
+    }
+
+    partial class User
+    {
+        public string Name { get; set; }
+        public void HI() { }
+    }
+}
